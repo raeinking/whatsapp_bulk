f99dd3787ec2065dc10de426c449b6ef33b84e17
raeinking <raeinking2@gmail.com>
2024-03-05 15:52:09 +0300

    commit

--- server/modul/users.js
+++ server/modul/users.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
+    const mongoose = require('mongoose');
+    const Schema = mongoose.Schema;
+    const ObjectId = Schema.ObjectId;
 
-const UsersSchema = new Schema({
-    id: ObjectId,
-    email: String,
-    isBand: Boolean,
-    date: Date,
-    expire_date: Date,
-});
+    const UsersSchema = new Schema({
+        id: ObjectId,
+        email: String,
+        isBand: Boolean,
+        date: Date,
+        expire_date: Date,
+    });
 
-const Users = mongoose.model('Users_whatsapp', UsersSchema);
+    const Users = mongoose.model('Users_whatsapp', UsersSchema);
 
-module.exports = {Users};
+    module.exports = {Users};
