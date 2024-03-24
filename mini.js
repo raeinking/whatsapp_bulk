004cd2621c0c3bdc6c463935a89e137e18d2804c
raeinking <raeinking2@gmail.com>
2024-03-24 10:26:33 +0300

    commit

--- mini.js
+++ mini.js
@@ -41,7 +41,6 @@
     
         // Handle dt-start event
         ipcMain.on('dt-start', (event, data) => {
-            console.log('Received dt-start event with data:', data);
             // You can perform any necessary actions with the data here
             // Pass the tableData directly to the 'newpage' event
             mainWindow.webContents.send('newpage');
@@ -119,10 +118,119 @@ function sleep(ms) {
 }
 
 // Event handler to send messages to numbers
+// ipcMain.on('dt-start', async (event, data) => {
+//     console.log(tableDataa);
+//     try {
+//         const { filePath, textContent } = data;
+
+//         // Ensure that the browser instance is available
+//         if (!browser) {
+//             throw new Error('Browser instance not available. Open Chrome first.');
+//         }
+
+//         // Get the existing pages
+//         const pages = await browser.pages();
+
+//         // If there are no open pages, throw an error
+//         if (pages.length === 0) {
+//             throw new Error('No open pages available.');
+//         }
+
+//         // Use the first page to send messages
+//         const page = pages[0];
+
+//         // Loop through each number to send a message
+//         for (let idx = 0; idx < tableDataa.length; idx++) {
+//             const number = tableDataa[idx].trim();
+//             if (number === "") {
+//                 continue;
+//             }
+            
+//             console.log(`${idx + 1}/${tableDataa.length} => Sending message to ${number}.`);
+
+//             try {
+//                 // Navigate to the URL for sending message to the current number
+//                 await page.goto(`https://web.whatsapp.com/send?phone=${number}&text=${encodeURIComponent(textContent)}`, { waitUntil: 'load' });
+
+//                 // Wait for the attachment button to appear with an increased timeout after page fully loaded
+                
+//                 // Click on the attachment button
+                
+//                 // Wait for the text input field to appear
+//                 // await page.waitForSelector('._3Uu1_', { timeout: 10000 }).catch(error => {
+//                     //     console.error('Failed lto find text input fied:', error);
+//                     // });
+                    
+//                 if (filePath) {
+//                     await page.waitForSelector('[data-icon="attach-menu-plus"]', { timeout: 15000 });
+//                     await page.evaluate(() => {
+//                         // Find the element with class 'bo8jc6qi' and click it
+//                         const attachMenuPlus = document.querySelector('[data-icon="attach-menu-plus"]')
+//                         if (attachMenuPlus) {
+//                             attachMenuPlus.click();
+//                         } else {
+//                             console.log("can't find this btn: ");
+//                         }
+//                     });
+                    
+//                     const extname = path.extname(filePath).toLowerCase();
+//                     let fileTypeSelector;
+
+//                     switch (extname) {
+//                         case '.jpg':
+//                         case '.jpeg':
+//                         case '.png':
+//                             fileTypeSelector = 'input[type="file"][accept*="image"]';
+//                             break;
+//                         case '.mp4':
+//                         case '.mov':
+//                             fileTypeSelector = 'input[type="file"][accept*="video"]';
+//                             break;
+//                         default:
+//                             throw new Error(`Unsupported file type: ${extname}`);
+//                     }
+
+//                     // Upload the file using the determined file type selector
+//                     const input = await page.$(fileTypeSelector);
+//                     await input.uploadFile(filePath);
+                    
+//                     // Wait for the text input field to appear
+//                     await page.waitForSelector('._ah9q', { timeout: 15000 }).catch(error => {
+//                         console.error('Failed lto find text input fied:', error);
+//                     });
+//                 }
+                
+//                 const sendButton = await page.waitForSelector('[data-icon="send"]', { timeout: 15000 }).catch(error => {
+//                     console.error('Failed to find send button:', error);
+//                 });
+
+//                 // Wait for the send button to become clickable
+
+//                 if (sendButton) {
+//                     await sleep(3000)
+//                     await sendButton.click();
+//                     await sleep(12000);
+//                 } else {
+//                     console.error("Send button not found.");
+//                 }
+//             } catch (error) {
+//                 console.error(`Error occurred while sending message to ${number}:`, error);
+//                 continue;
+//             } 
+//         }
+        
+//         // Close the browser after sending all messages
+//         await browser.close();
+//         // console.log('All messages sent. Browser closed.');
+        
+//     } catch (error) {
+//         console.error('An error occurred:', error);
+//         // Handle overall error if needed
+//     }
+// });
 ipcMain.on('dt-start', async (event, data) => {
-    console.log(tableDataa);
+    console.log(data.tableDataa);
     try {
-        const { filePath, textContent } = data;
 
         // Ensure that the browser instance is available
         if (!browser) {
@@ -140,90 +248,66 @@ ipcMain.on('dt-start', async (event, data) => {
         // Use the first page to send messages
         const page = pages[0];
 
-        // Loop through each number to send a message
-        for (let idx = 0; idx < tableDataa.length; idx++) {
-            const number = tableDataa[idx].trim();
-            if (number === "") {
+        // Loop through each object in tableDataa to send messages
+        for (let i = 0; i < tableDataa.length; i++) {
+            const { filePaths, textContent } = data.tableDataa[i];
+            if (!filePaths || filePaths.length === 0 || !textContent) {
+                console.error(`Missing filePaths or textContent at index ${i}. Skipping.`);
                 continue;
             }
             
-            console.log(`${idx + 1}/${tableDataa.length} => Sending message to ${number}.`);
+            console.log(`Sending message ${i + 1}/${tableDataa.length} with textContent: ${textContent}.`);
 
             try {
-                // Navigate to the URL for sending message to the current number
-                await page.goto(`https://web.whatsapp.com/send?phone=${number}&text=${encodeURIComponent(textContent)}`, { waitUntil: 'load' });
+                // Navigate to the URL for sending message
+                await page.goto(`https://web.whatsapp.com/send?text=${encodeURIComponent(textContent)}`, { waitUntil: 'load' });
 
-                // Wait for the attachment button to appear with an increased timeout after page fully loaded
-                
-                // Click on the attachment button
-                
                 // Wait for the text input field to appear
-                // await page.waitForSelector('._3Uu1_', { timeout: 10000 }).catch(error => {
-                    //     console.error('Failed lto find text input fied:', error);
-                    // });
-                    
-                if (filePath) {
-                    await page.waitForSelector('[data-icon="attach-menu-plus"]', { timeout: 15000 });
+                await page.waitForSelector('._3u328', { timeout: 15000 }).catch(error => {
+                    console.error('Failed to find text input field:', error);
+                });
+
+                // Loop through each filePath and upload the file
+                for (let j = 0; j < filePaths.length; j++) {
+                    const filePath = filePaths[j];
+                    console.log(`Uploading file ${j + 1}/${filePaths.length}: ${filePath}`);
+
+                    await page.waitForSelector('[data-icon="clip"]', { timeout: 15000 });
                     await page.evaluate(() => {
-                        // Find the element with class 'bo8jc6qi' and click it
-                        const attachMenuPlus = document.querySelector('[data-icon="attach-menu-plus"]')
-                        if (attachMenuPlus) {
-                            attachMenuPlus.click();
+                        // Find the element with class 'PVMjB' and click it to open file input
+                        const clipButton = document.querySelector('[data-icon="clip"]');
+                        if (clipButton) {
+                            clipButton.click();
                         } else {
-                            console.log("can't find this btn: ");
+                            console.log("Can't find clip button.");
                         }
                     });
-                    
-                    const extname = path.extname(filePath).toLowerCase();
-                    let fileTypeSelector;
 
-                    switch (extname) {
-                        case '.jpg':
-                        case '.jpeg':
-                        case '.png':
-                            fileTypeSelector = 'input[type="file"][accept*="image"]';
-                            break;
-                        case '.mp4':
-                        case '.mov':
-                            fileTypeSelector = 'input[type="file"][accept*="video"]';
-                            break;
-                        default:
-                            throw new Error(`Unsupported file type: ${extname}`);
-                    }
-
-                    // Upload the file using the determined file type selector
-                    const input = await page.$(fileTypeSelector);
+                    // Upload the file
+                    const input = await page.$('input[type="file"]');
                     await input.uploadFile(filePath);
-                    
-                    // Wait for the text input field to appear
-                    await page.waitForSelector('._ah9q', { timeout: 15000 }).catch(error => {
-                        console.error('Failed lto find text input fied:', error);
-                    });
                 }
-                
+
+                // Wait for the send button to become clickable
                 const sendButton = await page.waitForSelector('[data-icon="send"]', { timeout: 15000 }).catch(error => {
                     console.error('Failed to find send button:', error);
                 });
 
-                // Wait for the send button to become clickable
-
                 if (sendButton) {
-                    await sleep(3000)
+                    await sleep(3000);
                     await sendButton.click();
                     await sleep(12000);
                 } else {
                     console.error("Send button not found.");
                 }
             } catch (error) {
-                console.error(`Error occurred while sending message to ${number}:`, error);
+                console.error(`Error occurred while sending message at index ${i}:`, error);
                 continue;
             } 
         }
         
         // Close the browser after sending all messages
         await browser.close();
-        // console.log('All messages sent. Browser closed.');
-        
     } catch (error) {
         console.error('An error occurred:', error);
         // Handle overall error if needed
